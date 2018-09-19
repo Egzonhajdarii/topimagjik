864d592c9a8e89c9ad9af9474b12786230671969
student6 <student6@ICKs-Mac-mini-4.local>
2018-09-19 18:23:44 +0200

    First Commit

---
+++ Topi Magjik/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Topi Magjik
+//
+//  Created by student6 on 9/17/18.
+//  Copyright © 2018 Egzon hajdari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var fotoja: UIImageView!
+    let fotot:[String] = ["topi1","topi2","topi3","topi4","topi5",]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func btnLuaj(_ sender: Any) {
+        
+        let numriRandom:Int = Int(arc4random_uniform(5))
+        let fotojaRandom:String = fotot[numriRandom]
+        fotoja.image = UIImage.init(named: fotojaRandom)
+     
+        
+        
+    }
+    
+    }
+
+
+
+
